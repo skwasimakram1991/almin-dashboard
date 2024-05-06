d5c07afb3711ecd75b63a30ce56264feb0269ae1
Kaustav Sengupta <kaustav.sengupta@fortmindz.in>
2024-05-06 15:00:59 +0530

    setup fix

---
+++ js/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  // hide show password
+  $(".toggle-password").click(function () {
+    $(this).toggleClass("eye-close eye-open");
+    input = $(this).parent().find("input");
+    if (input.attr("type") == "password") {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
+  });
+  $(".height-1").matchHeight();
+  $(".height-2").matchHeight();
+});
+
+//! adjust Main-Content Height
+function adjustMainContentHeight() {
+  var headerHeight = document.querySelector(".topbar").offsetHeight;
+  var footerHeight = document.querySelector("footer").offsetHeight;
+  var windowHeight = window.innerHeight;
+
+  var mainContentHeight = windowHeight - headerHeight - footerHeight;
+  document.querySelector(".main-content").style.height =
+    mainContentHeight + "px";
+}
+
+window.onload = function () {
+  adjustMainContentHeight(); // Initial adjustment on page load
+};
+
+window.addEventListener("resize", adjustMainContentHeight); // Adjust main content height when window is resized
